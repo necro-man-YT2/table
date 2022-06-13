a2582cb2438b5ab7b09f0e0acfaefbeb93438914
necro-man-YT2 <k89781407726@gmail.com>
2022-06-13 12:00:29 +0300

    heroky apdate

--- Nikolay/settings.py
+++ Nikolay/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#-7kgb4jl)b$6j)2pw+0ri=&j7&-m0#r#h8=ie9k#7$h)+m-3u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
